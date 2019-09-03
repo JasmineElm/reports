224188c621a41ee6fda7d78b0c6b93797d656fab
kj6fi170m6e@riseup.net <kj6fi170m6e@riseup.net>
2019-09-03 19:57:08 +0100

    rearranged structure

--- compileReport.sh
+++ compileReport.sh
@@ -3,7 +3,7 @@ pandoc  report.md \
 		--toc \
 		--biblio=references.bib \
 		--pdf-engine=xelatex \
-		-o ../report.pdf \
+		-o report.pdf \
 		--template template/def.tex \
 		--highlight-style=tango \
 		--csl=template/harvard-cite-them-right.csl
